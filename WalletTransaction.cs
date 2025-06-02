c1fa91468e6c338f1d14cf5c6d4f8dc80d668a38
UAB\htunmyataung <htunmyataung643@gmail.com>
2025-06-02 11:10:49 +0630

    change author profile image

---
+++ WalletTransaction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Kispanadi.Common.Ddd.Objects;
+
+namespace UabPay.InternationalRemittance.Domain
+{   
+
+    public class WalletTransaction : IDomainObject
+    {
+        public DateTime TransactionDate { get; }
+        public string TransactionId { get; }
+        public string MobileNumber { get; }          
+        public string TransactionDescription { get; } 
+        public decimal Amount { get; }
+        public decimal Discount { get; }
+        public decimal Charges { get; }
+        public decimal NetAmount { get; }
+        public string TransferTo { get; }
+        public string ReceiverAppType { get; }
+
+        public WalletTransaction(
+            DateTime transactionDate,
+            string transactionId,
+            string mobileNumber,
+            string transactionDescription,
+            decimal amount,
+            decimal discount,
+            decimal charges,
+            decimal netAmount,
+            string transferTo,
+            string receiverAppType)
+        {
+            TransactionDate = transactionDate;
+            TransactionId = transactionId;
+            MobileNumber = mobileNumber;
+            TransactionDescription = transactionDescription;
+            Amount = amount;
+            Discount = discount;
+            Charges = charges;
+            NetAmount = netAmount;
+            TransferTo = transferTo;
+            ReceiverAppType = receiverAppType;
+        }
+    }
+
+
+    public class PaginatedWalletTransDomain : IDomainObject
+    {
+        public List<WalletTransaction> Transactions { get; set; } = new List<WalletTransaction>();
+        public int Skip { get; set; }
+        public int Limit { get; set; }
+        public int TotalCount { get; set; }
+    }
+
+    public class WalletTransReqDomain
+    {
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public string TransactionId { get; set; }
+        public int TransactionType { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
---
+++ WalletTransactionMapper.cs
@@ -0,0 +1,44 @@
+﻿using UabPay.InternationalRemittance.Domain;
+using UabPay.InternationalRemittance.Infrastructure.Persistence.Data;
+
+namespace UabPay.InternationalRemittance.Application.Mapper
+{
+    public class WalletTransactionMapper : IMapper<WalletTransaction, WalletTransactionDto>
+    {
+        public WalletTransaction ToDomain(WalletTransactionDto dbObject)
+        {
+            return dbObject is null ? null : new WalletTransaction(
+                dbObject.TransactionDate,
+                dbObject.TransactionId,
+                dbObject.MobileNumber,
+                dbObject.TransactionDescription,
+                dbObject.Amount,
+                dbObject.Discount,
+                dbObject.Charges,
+                dbObject.NetAmount,
+                dbObject.TransferTo,
+                dbObject.ReceiverAppType
+            );
+                
+        }
+
+        public WalletTransactionDto ToDbObject(WalletTransaction domainObject)
+        {
+            return domainObject is null ? null : new WalletTransactionDto
+            {
+                TransactionDate = domainObject.TransactionDate,
+                TransactionId = domainObject.TransactionId,
+                MobileNumber = domainObject.MobileNumber,
+                TransactionDescription = domainObject.TransactionDescription,
+                Amount = domainObject.Amount,
+                Discount = domainObject.Discount,
+                Charges = domainObject.Charges,
+                NetAmount = domainObject.NetAmount,
+                TransferTo = domainObject.TransferTo,
+                ReceiverAppType = domainObject.ReceiverAppType
+            };
+        }
+
+    }
+
+}
